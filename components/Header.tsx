2f3289f9a0a615cd64bdfe304c76f356a648019b
mistywynn10 <mistywynn10@gmail.com>
2026-06-05 14:48:33 -0400

    Set Up

---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+
+interface HeaderProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+    const router = useRouter();
+
+    const handleLogout = () => {
+        // handle logout in the future
+    }
+
+    return (
+        <div
+        className={twMerge(`
+            h-fit
+            bg-gradient-to-b
+            from-emerald-800
+            p-6
+            `)}
+        >
+            
+        </div>
+    )
+}
+
+export default Header;

23d6678f6c3b1ab95d3a6f543b6ffae8a8c45f5a
mistywynn10 <mistywynn10@gmail.com>
2026-07-23 17:52:52 -0400

    Media Item

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import useLoadImage from "@/hooks/useLoadImage";
+import usePlayer from "@/hooks/usePlayer";
 import { Song } from "@/types";
 import Image from "next/image";
 
@@ -14,6 +15,7 @@ const MediaItem: React.FC<MediaItemProps> = ({
     onClick
 }) => {
 
+    const player = usePlayer();
     const imageUrl = useLoadImage(data);
 
     const handleClick = () => {
@@ -21,7 +23,7 @@ const MediaItem: React.FC<MediaItemProps> = ({
             return onClick(data.id);
         }
 
-        // TODO: Default turn on player
+        return player.setId(data.id);
 
     }
 
